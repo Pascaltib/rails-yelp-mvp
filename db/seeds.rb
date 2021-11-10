bcc7f79c844c882eb6de662bbeff96eb1f8236ab
Pascal Tiberghien <pascaltib@gmail.com>
2021-11-10 15:47:40 +0100

    yelp done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning DB'
+Restaurant.destroy_all
+
+cat_arr = %w[chinese italian japanese french belgian]
+10.times do
+  temp = Restaurant.create(
+    {
+      name: Faker::Creature::Animal.name,
+      address: Faker::Address.street_address,
+      category: cat_arr.sample
+    }
+  )
+  5.times do
+    Review.create({ rating: rand(6), content: Faker::Quote.yoda , restaurant: temp})
+  end
+end
